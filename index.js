c4d8b18f61c487bbce1dc01674fc49f30ba2ad2a
Tekla Inashvili <tekla.inashvili.1@btu.edu.ge>
2022-12-02 20:05:49 +0400

    initial commit

--- index.js
+++ index.js
@@ -3,27 +3,9 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-blogSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+const Blog = require('./models/blog')
 
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = 'mongodb+srv://fullstack:fullstack@cluster0.jsfdfqg.mongodb.net/?retryWrites=true&w=majority'
-mongoose.connect(mongoUrl)
 
 app.use(cors())
 app.use(express.json())
@@ -67,7 +49,7 @@ app.delete('/api/blogs/:id', (request, response) => {
       .catch(error => console.log(error))
 })
 
-const PORT = 3003
+const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
